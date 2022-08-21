a305dbccfbde782226046898605f9f21551b4038
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-08-21 16:50:18 +0300

    Добавил все нужные поля.

--- ParamCalculator.cs
+++ ParamCalculator.cs
@@ -2,6 +2,21 @@ namespace ParamCalculator
 {
     public partial class ParamCalculator : Form
     {
+        /// <summary>  Сколько добавляется раз в Time времени </summary>
+        internal uint ScreenNumber = 0;
+
+        /// <summary>  Максимальное значение для ScreenNumber </summary>
+        internal uint MaxNumber = 10;
+
+        /// <summary>  Сколько добавляется раз в Time времени </summary>
+        internal uint Number = 10;
+
+        /// <summary>  Сколько добавить или убавить из ScreenNumber </summary>
+        internal uint DifferenceNumber = 0;
+
+        /// <summary> Раз во сколько секунд будет добавлятся Number к ScreenNumber </summary>
+        internal uint Time = 10;
+
         public ParamCalculator()
         {
             InitializeComponent();
