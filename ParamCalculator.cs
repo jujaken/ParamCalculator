cc1f7e36994eaf7fc75f3fc0449cda0685ab9da1
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-08-21 13:45:56 +0300

    Переименовал всё в ParamCalculator

--- ParamCalculator.cs
+++ ParamCalculator.cs
@@ -1,8 +1,8 @@
 namespace ParamCalculator
 {
-    public partial class Form1 : Form
+    public partial class ParamCalculator : Form
     {
-        public Form1()
+        public ParamCalculator()
         {
             InitializeComponent();
         }
