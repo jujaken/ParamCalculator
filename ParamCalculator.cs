23dbdfe19ee33a57f213a969f46e03ef8ebb8b3d
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-08-21 18:16:43 +0300

    Сделал таймер и ограничение на ScreenNumber

--- ParamCalculator.cs
+++ ParamCalculator.cs
@@ -3,7 +3,12 @@ namespace ParamCalculator
     public partial class ParamCalculator : Form
     {
         /// <summary>  Сколько добавляется раз в Time времени </summary>
-        internal int ScreenNumber = 0;
+        private int _ScreenNumber = 0;
+        internal int ScreenNumber
+        {
+            get => _ScreenNumber;
+            set => _ScreenNumber = value > MaxNumber ? (int)MaxNumber : value < 0 ? 0 : _ScreenNumber = value;
+        }
 
         /// <summary>  Максимальное значение для ScreenNumber </summary>
         internal uint MaxNumber = 10;
@@ -12,11 +17,16 @@ namespace ParamCalculator
         internal uint Number = 10;
 
         /// <summary> Раз во сколько секунд будет добавлятся Number к ScreenNumber </summary>
-        internal uint Time = 10;
+        internal uint Time = 1;
+
+        private System.Windows.Forms.Timer _Timer;
 
         public ParamCalculator()
         {
             InitializeComponent();
+
+            _Timer = new System.Windows.Forms.Timer();
+            _Timer.Tick += AddNumberWhenTimerAlarm;
         }
 
         #region Buttons
@@ -49,9 +59,27 @@ namespace ParamCalculator
 
         #endregion
 
+        private bool timerIsWork;
+
         private void RunTimerButton_Click(object sender, EventArgs e)
         {
+            if (timerIsWork)
+            {
+                timerIsWork = false;
+                _Timer.Stop();
+            }
+            else
+            {
+                timerIsWork = true;
+                _Timer.Interval = (int)Time * 1000;
+                _Timer.Start();
+            }
+        }
 
+        private void AddNumberWhenTimerAlarm(object? myObject, EventArgs myEventArgs)
+        {
+            ScreenNumber += (int)Number;
+            ScreenNumberLabel.Text = ScreenNumber.ToString();
         }
 
         #endregion
