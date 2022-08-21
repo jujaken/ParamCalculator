eca108a318c1cd47d1690291f1ac14cad1914e6a
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-08-21 18:21:54 +0300

    Изменил оформление аля TT2D

--- ParamCalculator.Designer.cs
+++ ParamCalculator.Designer.cs
@@ -48,15 +48,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label1.Location = new System.Drawing.Point(12, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(80, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Название:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label2.Location = new System.Drawing.Point(12, 95);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(95, 20);
@@ -66,29 +71,38 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label3.Location = new System.Drawing.Point(12, 166);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(145, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Сколько добавлять:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label4.Location = new System.Drawing.Point(250, 166);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(200, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Через сколько (в секундах):";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label5.Location = new System.Drawing.Point(12, 231);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(200, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "Сколько отнять/прибавить:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // NameCharacterTextBox
             // 
@@ -101,6 +115,7 @@
             // 
             this.ScreenNumberLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ScreenNumberLabel.Font = new System.Drawing.Font("Segoe UI", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ScreenNumberLabel.ForeColor = System.Drawing.Color.DarkCyan;
             this.ScreenNumberLabel.Location = new System.Drawing.Point(183, 33);
             this.ScreenNumberLabel.Name = "ScreenNumberLabel";
             this.ScreenNumberLabel.Size = new System.Drawing.Size(287, 113);
@@ -141,6 +156,7 @@
             // 
             // TakeButton
             // 
+            this.TakeButton.ForeColor = System.Drawing.Color.DarkCyan;
             this.TakeButton.Location = new System.Drawing.Point(250, 252);
             this.TakeButton.Name = "TakeButton";
             this.TakeButton.Size = new System.Drawing.Size(104, 29);
@@ -151,6 +167,7 @@
             // 
             // AddButton
             // 
+            this.AddButton.ForeColor = System.Drawing.Color.DarkCyan;
             this.AddButton.Location = new System.Drawing.Point(366, 252);
             this.AddButton.Name = "AddButton";
             this.AddButton.Size = new System.Drawing.Size(104, 29);
@@ -161,6 +178,7 @@
             // 
             // RunTimerButton
             // 
+            this.RunTimerButton.ForeColor = System.Drawing.Color.DarkCyan;
             this.RunTimerButton.Location = new System.Drawing.Point(12, 335);
             this.RunTimerButton.Name = "RunTimerButton";
             this.RunTimerButton.Size = new System.Drawing.Size(458, 29);
@@ -172,17 +190,20 @@
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label6.Location = new System.Drawing.Point(12, 312);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(193, 20);
             this.label6.TabIndex = 15;
             this.label6.Text = "Статус таймера: выключен";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // ParamCalculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.BackColor = System.Drawing.Color.DarkCyan;
             this.ClientSize = new System.Drawing.Size(482, 383);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.RunTimerButton);
